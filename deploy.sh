de0842dfa0426ed915464597a94dc927c341cd27
jturk998 <37187671+jturk998@users.noreply.github.com>
2021-02-12 17:24:01 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -187,7 +187,6 @@ then
         --tags Framework=sdlf \
         --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND" \
         --region ${REGION} \
-        --parameters ParameterKey=PermBound,ParameterValue=${PERM_BOUND}\
         --profile ${CHILD_PROFILE}
     echo "Waiting for stack to be created ..."
     aws cloudformation wait stack-create-complete --profile ${CHILD_PROFILE} --region ${REGION} --stack-name ${STACK_NAME}
